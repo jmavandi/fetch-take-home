b89ed19d33c12bc2c642bd610999a71c6e67b5f1
Jonathan Mavandi <jonathan.mavandi@gmail.com>
2025-02-06 17:19:43 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen p-8">
+      <main className="flex flex-col gap-8">
+        <h1>Welcome to Fetch</h1>
+      </main>
+    </div>
+  );
+}
